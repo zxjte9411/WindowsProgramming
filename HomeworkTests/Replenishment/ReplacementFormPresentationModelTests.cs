16ee2c8025a2c57c3d2a1b5095b206a035673a73
zxjte9411 <a5852241@gmail.com>
2019-11-13 20:23:03 +0800

    測試 ReplacementFormPM

---
+++ HomeworkTests/Replenishment/ReplacementFormPresentationModelTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Homework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Tests
+{
+    [TestClass()]
+    public class ReplacementFormPresentationModelTests
+    {
+        ReplacementFormPresentationModel replacementFormPresentationModel;
+        Model model;
+        Category category;
+        Product product;
+        bool flagTest;
+
+        // test event
+        private void EventTest()
+        {
+            flagTest = true;
+        }
+
+        // test Initialize
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            model = new Model();
+            replacementFormPresentationModel = new ReplacementFormPresentationModel(model);
+            category = new Category("test", 1);
+            product = new Product("3700xtest", category, "10000", "8c16txtest", Constant.RESOURCE_PATH + "CPU/0.jpg", "3");
+            flagTest = false;
+            model.ProductList.Add(product);
+        }
+        // test ReplacementFormPresentationModel
+        [TestMethod()]
+        public void TestReplacementFormPresentationModel()
+        {
+            Assert.AreEqual(replacementFormPresentationModel.Model, model);
+        }
+
+        // test HandleStockQuantityChange
+        [TestMethod()]
+        public void TestHandleStockQuantityChange()
+        {
+            replacementFormPresentationModel.Model._stockQuantityProductChangeEvent += EventTest;
+            replacementFormPresentationModel.HandleStockQuantityChange(model.ProductList.Count - 1, "5");
+            Assert.AreEqual(model.ProductList.Last().Quantity, "8");
+            Assert.AreEqual(flagTest, true);
+        }
+    }
+}
